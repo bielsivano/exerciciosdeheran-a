b092edd25ae0f361ec14312bcd9e7b75dd9bfb89
Gabriel Silvano Ribeiro <140166480+bielsivano@users.noreply.github.com>
2023-10-30 14:54:23 -0300

    Update pessoa.java

--- pessoa.java
+++ pessoa.java
@@ -8,7 +8,8 @@ public class pessoa {
    public  pessoa(String _nome, String telefone, String endereco){
        this.nome = _nome;
        this.telefone = telefone;
-       this.endereco = telefone;
+       this.endereco = endereco;
+      
 
    }
 
